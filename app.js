ddc6cdbcaec98980aab0662b00dc2025a1c84c6a
nicola00 <nicolajobs00@gmail.com>
2023-02-13 15:31:11 +0100

    FEAT create five random number in 30 second

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
-//Visualizzare in pagina 5 numeri casuali con alert.
+// Visualizzare in pagina 5 numeri casuali con alert.
 // Dopo che l’utente ha chiuso l’alert fate partire un timer di 30 secondi.
 // Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali numeri sono stati individuati tramite console.log
+
+
+let timer = document.getElementById('timer')
+let text = document.querySelector('.title')
+
+let clockWord
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+let numRandom = getRandomIntInclusive(1, 100)
+
+for (let i = 0; i < 5; i++){
+    numRandom = getRandomIntInclusive(1, 100)
+    
+    alert(numRandom)
+    console.log(numRandom)
+}
+
+
+let counter = 30
+
+clockWord = setInterval (function() {
+    counter--
+    
+    timer.innerHTML = counter
+
+    if (counter === 0){
+        clearInterval(clockWord)
+    }
+
+    console.log( counter)
+}, 1000)
+
