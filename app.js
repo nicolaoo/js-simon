a7a2aca35151f5f1710cbf6dedb83db388303c88
nicola00 <nicolajobs00@gmail.com>
2023-02-14 17:39:20 +0100

    FIX function numeriCasuali

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Visualizzare in pagina 5 numeri casuali con alert.
 // Dopo che l’utente ha chiuso l’alert fate partire un timer di 30 secondi.
-// Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali numeri sono stati individuati tramite console.log
+// Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta, 
+// i numeri che ha visto precedentemente, tramite il prompt(). 
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti 
+// e quali numeri sono stati individuati tramite console.log
 
 
 let timer = document.getElementById('timer')
 let text = document.querySelector('.title')
+let numRandom = getRandomIntInclusive(1, 100)
 
-let clockWord
-
+const numeriCasuali(5, 1, 100)
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -15,27 +18,44 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); 
 }
 
-let numRandom = getRandomIntInclusive(1, 100)
+function numeriCasuali(length, min, max){
+    let numbersRandom = []
 
-for (let i = 0; i < 5; i++){
-    numRandom = getRandomIntInclusive(1, 100)
-    
-    alert(numRandom)
-    console.log(numRandom)
+    while(numbersRandom.length < length){
+        const num = getRandomIntInclusive(min, max)
+    }
+
+    return numbersRandom
 }
 
 
-let counter = 30
-
-clockWord = setInterval (function() {
-    counter--
+// for (let i = 0; i < 5; i++){
+//         numUser = parseInt(prompt('inserisci i numeri'))
+        
+//         arrayUser.push(numUser)
+        
+        
+//         // } else{
+//         //     console.log('hai perso')
+//         // }
+//        console.log()
+//     }
+ 
+//     if (numRandom.includes(arrayUser)){
+//             console.log(numUser, arrayUser[i],'hai vinto')
+//         }
+        
+        
+        
+        
+// clockWord = setInterval (function() {
+//     counter--
     
-    timer.innerHTML = counter
-
-    if (counter === 0){
-        clearInterval(clockWord)
-    }
-
-    console.log( counter)
-}, 1000)
-
+//     timer.innerHTML = counter
+    
+//     if (counter === 0){
+//             clearInterval(clockWord)
+//         }
+    
+//         console.log( counter)
+// }, 1000)
